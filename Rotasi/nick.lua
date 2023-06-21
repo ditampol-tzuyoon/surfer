3b92cf52451dbe6fcbe8f7906611f49f3efb4ad8
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2023-06-21 21:36:00 +0700

    Update nick.lua

--- Rotasi/nick.lua
+++ Rotasi/nick.lua
@@ -56,5 +56,6 @@ ListPembeli = {
     {iddc = "803571303625785395", nick = "w", aktif = 0},
     {iddc = "553382597070290965", nick = "x", aktif = 0},
     {iddc = "627084072329674769", nick = "y", aktif = 0},
-    {iddc = "500858459059781632", nick = "z", aktif = 0}
+    {iddc = "500858459059781632", nick = "z", aktif = 0},
+    {iddc = "438947974429671424", nick = "aa", aktif = 0}
 }
