83fd9bc1c457afa1349f7566b5e9729707dc9e08
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2024-01-07 02:23:46 +0800

    Update opsional.lua

--- Rotasi/opsional.lua
+++ Rotasi/opsional.lua
@@ -1,6 +1,6 @@
--- while true do
---     Tegs("SCRIPT INI SUDAH EXPIRED!")
---     sleep(6000000)
--- end
+while true do
+      Tegs("SCRIPT INI SUDAH EXPIRED!")
+      sleep(6000000)
+end
 
-Tegs("Mohon Maaf Hostingan Bot Discord Mati mulu, Sebagai Opsional Sc ini Tanpa Token")
+-- Tegs("Mohon Maaf Hostingan Bot Discord Mati mulu, Sebagai Opsional Sc ini Tanpa Token")
